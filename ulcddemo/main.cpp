b3ee6f36eaad839e6d14f51d97c1c53a6ee42f39
barry <410547@gm.tnfsh.tn.edu.tw>
2020-04-01 17:41:13 +0800

    2

--- ulcddemo/main.cpp
+++ ulcddemo/main.cpp
@@ -2,10 +2,10 @@
 
 #include "uLCD_4DGL.h"
 
-
-uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
-
-
+DigitalIn sample(D0);
+uLCD_4DGL uLCD(D1, D7, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D6);
+Serial pc( USBTX, USBRX );
 int main()
 
 {
@@ -21,4 +21,18 @@ int main()
     uLCD.color(RED);
 
     uLCD.rectangle(50,50, 100,90,0x0000FF);
+    /***********************/
+    PWM1.period(0.001);
+    PWM1 = 0;
+    while(1){
+     //   pc.printf("%1.3f\r\n",sample );
+        if(PWM1<1){
+            PWM1=PWM1+0.1;
+            wait(0.1);
+        }
+        else{
+            PWM1=PWM1-0.1;
+            wait(0.1);
+        }
+    }
 }
